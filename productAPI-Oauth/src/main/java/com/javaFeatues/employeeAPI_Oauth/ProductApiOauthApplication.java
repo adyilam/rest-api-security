3f623b1ff72826695eef0f778a84beac0fbb6bb2
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2025-03-05 17:57:46 -0500

    directory name changed

---
+++ productAPI-Oauth/src/main/java/com/javaFeatues/employeeAPI_Oauth/ProductApiOauthApplication.java
@@ -0,0 +1,13 @@
+package com.javaFeatues.employeeAPI_Oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiOauthApplication.class, args);
+    }
+
+}
---
+++ productAPI-Oauth/src/main/java/com/javaFeatues/employeeAPI_Oauth/web/ProductController.java
@@ -0,0 +1,46 @@
+package com.javaFeatues.employeeAPI_Oauth.web;
+
+import com.javaFeatues.employeeAPI_Oauth.domain.Product;
+import com.javaFeatues.employeeAPI_Oauth.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable UUID id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable UUID id, @RequestBody Product updatedProduct) {
+        try {
+            Product product = productService.updateProduct(id, updatedProduct);
+            return ResponseEntity.ok(product);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+}
