ea62dd8147bfcf0c5ebc1103411dea4e371f9546
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2025-03-05 17:40:43 -0500

    Produsct microservice with spring boot Oauth security

---
+++ employeeAPI-Oauth/src/test/java/com/javaFeatues/employeeAPI_Oauth/EmployeeApiOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaFeatues.employeeAPI_Oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
